f15e425476c56fa5977ed674ac5b6e386763fe90
faitsse <2138889191@qq.com>
2022-06-24 01:11:14 +0800

    chore: init

---
+++ src/components/Tweet.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { isClient } from '@vueuse/core'
+import { isDark } from '../logics'
+
+if (isClient) {
+  onMounted(() => {
+  // @ts-expect-error
+    window?.twttr.widgets.load()
+  })
+}
+</script>
+
+<template>
+  <div>
+    <blockquote class="twitter-tweet" :data-theme="isDark ? 'dark' : 'light'">
+      <slot />
+    </blockquote>
+  </div>
+</template>
