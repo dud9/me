4e027f32e4871ff195995965eeb9f6360f505bea
faitsse <2138889191@qq.com>
2022-06-24 00:58:57 +0800

    chore: init

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,15 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+/// <reference types="vite-plugin-pages/client" />
+
+declare module '*.md' {
+  import { type DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,83 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Markdown from 'vite-plugin-md'
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetIcons, presetUno, presetWind } from 'unocss'
+import Prism from 'markdown-it-prism'
+import LinkAttributes from 'markdown-it-link-attributes'
+
+const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue()]
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  plugins: [
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
+    }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      dts: 'autoImports.d.ts',
+      dirs: [
+        'src/composables',
+      ],
+    }),
+
+    // your plugin installation
+    Components({
+      resolvers: [
+
+      ],
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss({
+      shortcuts: {
+        'icon-btn': 'op30 hover:op100]',
+      },
+      presets: [
+        presetAttributify(),
+        presetUno(),
+        presetIcons({
+          scale: 1.2,
+        }),
+        presetWind(),
+      ],
+    }),
+
+    // https://github.com/antfu/vite-plugin-md
+    Markdown({
+      wrapperClasses: markdownWrapperClasses,
+      headEnabled: true,
+      markdownItSetup(md) {
+        // https://prismjs.com/
+        md.use(Prism)
+        md.use(LinkAttributes, {
+          matcher: (link: string) => /^https?:\/\//.test(link),
+          attrs: {
+            target: '_blank',
+            rel: 'noopener',
+          },
+        })
+      },
+    }),
+  ],
 })
