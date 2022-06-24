6038c4be97349c4854f26adbdf648f06b4857a5d
faitsse <2138889191@qq.com>
2022-06-24 19:52:29 +0800

    chore: update

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -6,7 +6,7 @@ const headerFixed = fixedHeader
 </script>
 
 <template>
-  <n-layout :native-scrollbar="false" h-screen w-screen>
+  <n-layout :native-scrollbar="false" h-screen w-screen min-h-screen>
     <n-layout-header bordered :class="headerFixed ? 'fixed top-0 left-0' : ''">
       <TheNav w-full class="h-[4.5rem]" />
     </n-layout-header>
@@ -15,12 +15,12 @@ const headerFixed = fixedHeader
       :position="headerFixed ? 'absolute' : 'static'"
       :class="headerFixed ? 'mt-[4.5rem]' : ''"
     >
-      <n-layout-content bg="[#F5F5F5] dark:[#101014]">
+      <n-layout-content>
         <div flex justify-center px-7 py-10>
           <RouterView />
         </div>
       </n-layout-content>
-      <n-layout-footer bg="[#F5F5F5] dark:[#101014]">
+      <n-layout-footer bg="white dark:[#101014]">
         <TheFoot w-full h-50px />
       </n-layout-footer>
       <n-back-top v-if="headerFixed" :right="50" />
