b5657e6cee690597b0bbc32623ab7176aaea82ad
faitsse <2138889191@qq.com>
2022-06-24 15:15:21 +0800

    chore: update

--- src/components/FixHeader.vue
+++ src/components/FixHeader.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const toggle = toggleFixedHeader
+</script>
+
 <template>
   <div fixed top-8rem right-50px lt-md:hidden>
     <n-tooltip
@@ -5,7 +9,7 @@
       trigger="hover"
     >
       <template #trigger>
-        <n-button text class="text-2xl">
+        <n-button text class="text-2xl" @click="toggle()">
           <div i-bi-pin-angle op-50 hover:op-100 focus:op-100 />
         </n-button>
       </template>
---
+++ src/composables/fixHeader.ts
@@ -0,0 +1,4 @@
+const { bool, toggle } = useBoolean()
+
+export const fixedHeader = bool
+export const toggleFixedHeader = toggle
