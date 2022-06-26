ea242384e5be887f8b7d01d26fd488996bc8b032
faitsse <2138889191@qq.com>
2022-06-26 22:47:47 +0800

    chore: update subnav

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
+import type { SubNavType } from '~/types'
+
 const inactiveStyle = 'opacity-20 hover:opacity-50'
 function isActive(type: string) {
   return type === unref(activeSubNav)
+}
+function go2(type: SubNavType) {
+  changeActiveSubNav(type)
 }
 </script>
 
 <template>
   <div prose m-auto mb-8 select-none>
     <div mb-0 flex="~ gap-3" text-3xl>
-      <router-link to="/posts/front" class="!border-none !font-400" :class="isActive('front') ? '' : inactiveStyle">
+      <a class="!border-none !font-400" :class="isActive('front') ? '' : inactiveStyle" @click="go2('front')">
         前端
-      </router-link>
-      <router-link to="/posts/end" class="!border-none !font-400" :class="isActive('end') ? '' : inactiveStyle">
+      </a>
+      <a class="!border-none !font-400" :class="isActive('end') ? '' : inactiveStyle" @click="go2('end')">
         后端
-      </router-link>
-      <router-link to="/posts/logic" class="!border-none !font-400" :class="isActive('logic') ? '' : inactiveStyle">
+      </a>
+      <a class="!border-none !font-400" :class="isActive('logic') ? '' : inactiveStyle" @click="go2('logic')">
         算法
-      </router-link>
-      <router-link to="/posts/life" class="!border-none !font-400" :class="isActive('life') ? '' : inactiveStyle">
+      </a>
+      <a class="!border-none !font-400" :class="isActive('life') ? '' : inactiveStyle" @click="go2('life')">
         生活
-      </router-link>
+      </a>
     </div>
   </div>
 </template>
