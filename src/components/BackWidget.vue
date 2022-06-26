0989ca8974c59b9be1417bc6cf4ce91afef544de
faitsse <2138889191@qq.com>
2022-06-26 23:04:40 +0800

    chore: update

--- src/components/BackWidget.vue
+++ src/components/BackWidget.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 const route = useRoute()
+const destination = computed(() => {
+  if (!route.path)
+    return '/'
+  const split = route.path.split('/')
+  return split.length > 2
+    ? split.slice(0, 2).join('/')
+    : split.slice(0, -1).join('/') || '/'
+})
 </script>
 
 <template>
   <router-link
-    :to="route.path.split('/').slice(0, -1).join('/') || '/'"
+    :to="destination"
     font-mono no-underline opacity-50 hover:opacity-75
   >
     cd ..
