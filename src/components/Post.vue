660ba73fed6b047ae43f944d70e3a164add0949e
faitsse <2138889191@qq.com>
2022-06-26 22:48:55 +0800

    chore: update

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,9 +1,76 @@
 <script setup lang="ts">
-
+const { frontmatter } = defineProps({
+  frontmatter: {
+    type: Object,
+    required: true,
+  },
+})
+const format = formatDate
+const router = useRouter()
+const route = useRoute()
+const content = ref<HTMLDivElement>()
+onMounted(() => {
+  const navigate = () => {
+    if (location.hash) {
+      document.querySelector(decodeURIComponent(location.hash))
+        ?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+  const handleAnchors = (
+    event: MouseEvent & { target: HTMLElement },
+  ) => {
+    const link = event.target.closest('a')
+    if (
+      !event.defaultPrevented
+      && link
+      && event.button === 0
+      && link.target !== '_blank'
+      && link.rel !== 'external'
+      && !link.download
+      && !event.metaKey
+      && !event.ctrlKey
+      && !event.shiftKey
+      && !event.altKey
+    ) {
+      const url = new URL(link.href)
+      if (url.origin !== window.location.origin)
+        return
+      event.preventDefault()
+      const { pathname, hash } = url
+      if (hash && (!pathname || pathname === location.pathname)) {
+        window.history.replaceState({}, '', hash)
+        navigate()
+      }
+      else {
+        router.push({ path: pathname, hash })
+      }
+    }
+  }
+  useEventListener(window, 'hashchange', navigate)
+  useEventListener(content.value!, 'click', handleAnchors, { passive: false })
+  navigate()
+  setTimeout(navigate, 500)
+})
 </script>
 
 <template>
-  <div>
-    1
+  <div flex="~ col" border="1 green">
+    <div v-if="frontmatter.display ?? frontmatter.title" class="prose m-auto mb-8">
+      <h1 class="mb-0">
+        {{ frontmatter.display ?? frontmatter.title }}
+      </h1>
+      <p v-if="frontmatter.date" class="opacity-50 !-mt-2">
+        {{ format(frontmatter.date) }} <span v-if="frontmatter.duration">· {{ frontmatter.duration }}</span>
+      </p>
+      <p v-if="frontmatter.subtitle" class="opacity-50 !-mt-6 italic">
+        {{ frontmatter.subtitle }}
+      </p>
+    </div>
+    <article ref="content">
+      <slot />
+    </article>
+    <div v-if="!['index', 'all'].includes(route.name as string)" class="w-full prose m-auto mt-8 mb-8">
+      <BackWidget />
+    </div>
   </div>
 </template>
