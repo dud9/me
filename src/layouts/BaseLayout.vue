62cfc20a3e833f1729498c0396c0171e2d6e5ddd
faitsse <2138889191@qq.com>
2022-06-26 23:39:38 +0800

    chore: update

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -3,14 +3,26 @@ import TheNav from './TheNav.vue'
 import TheFoot from './TheFoot.vue'
 
 const headerFixed = fixedHeader
+const refBaseWrapper = ref()
+const refContentWrapper = ref()
+const route = useRoute()
+watch(() => route.path, (val, old) => {
+  if (!old.startsWith(val)) {
+    const target = headerFixed
+      ? refContentWrapper
+      : refBaseWrapper
+    target.value?.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+})
 </script>
 
 <template>
-  <n-layout :native-scrollbar="false" h-screen w-screen min-h-screen>
+  <n-layout ref="refBaseWrapper" :native-scrollbar="false" h-screen w-screen min-h-screen>
     <n-layout-header bordered :class="headerFixed ? 'fixed top-0 left-0' : ''">
       <TheNav w-full class="h-[4.5rem]" />
     </n-layout-header>
     <n-layout
+      ref="refContentWrapper"
       :native-scrollbar="false"
       :position="headerFixed ? 'absolute' : 'static'"
       :class="headerFixed ? 'mt-[4.5rem]' : ''"
@@ -19,7 +31,7 @@ const headerFixed = fixedHeader
         <div flex="~ col" justify-center px-7 py-10>
           <RouterView v-slot="{ Component, route }">
             <Transition name="fade-slide" mode="out-in" appear>
-              <Component :is="Component" :key="route.fullPath" />
+              <component :is="Component" :key="route.fullPath" />
             </Transition>
           </RouterView>
         </div>
