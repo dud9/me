024878e16807effc9095c091e410285807007540
faitsse <2138889191@qq.com>
2022-06-26 22:44:40 +0800

    chore: add AppLink

---
+++ src/components/AppLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const {
+  to,
+} = defineProps<{
+  to: string
+}>()
+const isExternalLink = computed(() => {
+  return typeof to === 'string' && to.startsWith('http')
+})
+</script>
+
+<template>
+  <a v-if="isExternalLink" v-bind="$attrs" :href="to" target="_blank">
+    <slot />
+  </a>
+  <router-link v-else v-bind="$props">
+    <slot />
+  </router-link>
+</template>
