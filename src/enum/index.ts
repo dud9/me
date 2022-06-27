840713075dddb25ecc4bbb7db3224f68f1d91485
faitsse <2138889191@qq.com>
2022-06-27 10:46:03 +0800

    chore: update

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -1,8 +1,8 @@
 export enum EnumAppMeta {
-  avatar = 'src/assets/avatar.jpg',
-  avatar2 = 'src/assets/avatar2.jpg',
-  qqScanCode = 'src/assets/qq-scan-code.jpg',
-  wechatPay = 'src/assets/wechat-pay-code.jpg',
+  avatar = '/src/assets/avatar.jpg',
+  avatar2 = '/src/assets/avatar2.jpg',
+  qqScanCode = '/src/assets/qq-scan-code.jpg',
+  wechatPay = '/src/assets/wechat-pay-code.jpg',
 }
 
 export enum EnumStorageKey {
