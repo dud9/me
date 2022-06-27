f048068e487993c95887fd81b5c873ff85359b52
faitsse <2138889191@qq.com>
2022-06-27 10:48:27 +0800

    chore: update

--- src/composables/useImageNote.ts
+++ src/composables/useImageNote.ts
@@ -3,9 +3,9 @@ import { NAvatar, NImage } from 'naive-ui'
 export function useImageNote({
   title = '',
   description = '',
-  imageSrc = 'src/assets/avatar2.jpg',
+  imageSrc = '/src/assets/avatar2.jpg',
   metaDate = dayJs(new Date()).format('YYYY-MM-DD HH:mm:ss'),
-  avatar = 'src/assets/avatar.jpg',
+  avatar = '/src/assets/avatar.jpg',
   hasLeaveMessage = false,
   leaveMessage = 'Thank U~',
   onClose = () => {},
