50640675846e4667513d8bf9252f9c4dc5ba94af
faitsse <2138889191@qq.com>
2022-06-27 09:25:02 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -78,14 +78,11 @@ export default defineConfig({
       extendRoute(route) {
         const path = resolve(__dirname, route.component.slice(1))
 
-        // if (!path.includes('projects.md')) {
-        //   const md = fs.readFileSync(path, 'utf-8')
-        //   const { data } = matter(md)
-        //   route.meta = Object.assign(route.meta || {}, { frontmatter: data })
-        // }
-        const md = fs.readFileSync(path, 'utf-8')
-        const { data } = matter(md)
-        route.meta = Object.assign(route.meta || {}, { frontmatter: data })
+        if (!path.includes('projects.md')) {
+          const md = fs.readFileSync(path, 'utf-8')
+          const { data } = matter(md)
+          route.meta = Object.assign(route.meta || {}, { frontmatter: data })
+        }
         return route
       },
     }),
@@ -122,4 +119,13 @@ export default defineConfig({
       },
     }),
   ],
+
+  build: {
+    rollupOptions: {
+      onwarn(warning, next) {
+        if (warning.code !== 'UNUSED_EXTERNAL_IMPORT')
+          next(warning)
+      },
+    },
+  },
 })
