f0cc59c3ac202f0bee2ec1c11bcd5ac89e4540b5
faitsse <2138889191@qq.com>
2022-06-30 20:25:51 +0800

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
+import './styles/prose.css'
+import './styles/markdown.css'
 import 'uno.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
 
 function init(app: any) {
   Object.values(import.meta.globEager('./modules/*.ts'))
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import fs from 'fs-extra'
-import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
