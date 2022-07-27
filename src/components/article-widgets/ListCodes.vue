4890956008af57f67065e11d34d86e5b1f25122f
faitsse <2138889191@qq.com>
2022-07-27 14:16:56 +0800

    chore: update

--- src/components/article-widgets/ListCodes.vue
+++ src/components/article-widgets/ListCodes.vue
@@ -20,6 +20,21 @@ const posts = computed<Post[]>(() => {
     })) || []
 })
 
+const postsByYear = computed<Record<string, Post[]>>(() => {
+  const obj: Record<string, Post[]> = {}
+  const _post = posts.value
+  if (_post.length === 0)
+    return obj
+  _post.forEach((i) => {
+    const year = useDayJs(i.date).format('YYYY')
+    const yearPosts = Object.prototype.hasOwnProperty.call(obj, year)
+      ? [...obj[year], i]
+      : [i]
+    obj[year] = yearPosts
+  })
+  return obj
+})
+
 const refCodeCnt = ref()
 const codeCntLabel = computed(() => {
   return '累计完成'
@@ -32,9 +47,9 @@ onMounted(() => {
 })
 function getPostType(difficulty: 'simple' | 'medium' | 'hard' = 'simple') {
   return {
-    simple: 'success',
-    medium: 'warning',
-    hard: 'error',
+    simple: ['success', 'Simple'],
+    medium: ['warning', 'Medium'],
+    hard: ['error', 'Hard'],
   }[difficulty]
 }
 </script>
@@ -56,40 +71,48 @@ function getPostType(difficulty: 'simple' | 'medium' | 'hard' = 'simple') {
       </n-statistic>
       <ListCodesProgress v-bind="{ total: 30, simple: 5, medium: 10, hard: 15 }" />
     </div>
-    <div mt-10px>
-      <div class="group">
-        <n-h2 prefix="bar" type="info" align-text>
-          2022 <span text-14px hidden group-hover:inline-block>(3)</span>
-        </n-h2>
-      </div>
-      <n-timeline>
-        <n-timeline-item
-          v-for="post, idx in posts"
-          :key="idx"
-          :type="getPostType(post.difficulty)"
-        >
-          <template #header>
-            <div flex items-center>
-              <span mr-3>{{ useDayJs(post.date).format('MM/DD') }}</span>
-              <span mr-2>{{ post.title }}</span>
-              <n-tag v-if="post.difficulty === 'simple'" type="success" size="small" round>
-                Simple
-              </n-tag>
-              <n-tag v-if="post.difficulty === 'medium'" type="warning" size="small" round>
-                Medium
-              </n-tag>
-              <n-tag v-if="post.difficulty === 'hard'" type="error" size="small" round>
-                Hard
-              </n-tag>
-              <div v-if="post.tags?.length" flex-inline items-center ml-4>
-                <n-tag v-for="tag, idx in post.tags" :key="idx" type="info" :bordered="false" size="small" mr-2 cursor-pointer>
-                  {{ tag }}
+    <div v-if="Object.keys(postsByYear).length" mt-30px>
+      <template
+        v-for="[key, posts] in Object.entries(postsByYear)
+          .sort((a, b) => +Number(b[0]) - +Number(a[0]))"
+        :key="key"
+      >
+        <div class="group">
+          <n-h2 prefix="bar" type="info" align-text>
+            {{ key }} <span text-14px hidden group-hover:inline-block>({{ posts.length || 0 }})</span>
+          </n-h2>
+        </div>
+        <n-timeline mb-5 :icon-size="24">
+          <n-timeline-item
+            v-for="post, idx in posts"
+            :key="idx" class="flex items-center"
+          >
+            <template #icon>
+              <div v-if="post.difficulty === 'simple'" i-twemoji-zany-face />
+              <div v-if="post.difficulty === 'medium'" i-twemoji-confounded-face />
+              <div v-if="post.difficulty === 'hard'" i-twemoji-loudly-crying-face />
+            </template>
+            <template #header>
+              <div flex items-center>
+                <AppLink
+                  :to="post.path" class="mr-2 item"
+                >
+                  <span mr-3>{{ useDayJs(post.date).format('MM/DD') }}</span>
+                  <span>{{ post.title }}</span>
+                </AppLink>
+                <n-tag :type="getPostType(post.difficulty)[0]" size="small" round>
+                  {{ getPostType(post.difficulty)[1] }}
                 </n-tag>
+                <div v-if="post.tags?.length" flex-inline items-center ml-4>
+                  <n-tag v-for="tag, idx in post.tags" :key="idx" type="info" :bordered="false" size="small" mr-2 cursor-pointer>
+                    {{ tag }}
+                  </n-tag>
+                </div>
               </div>
-            </div>
-          </template>
-        </n-timeline-item>
-      </n-timeline>
+            </template>
+          </n-timeline-item>
+        </n-timeline>
+      </template>
     </div>
   </div>
 </template>
