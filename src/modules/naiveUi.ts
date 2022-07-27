558b44cd691bb00d5cb8496d75c644beab419783
faitsse <2138889191@qq.com>
2022-07-27 17:09:39 +0800

    chore: update

--- src/modules/naiveUi.ts
+++ src/modules/naiveUi.ts
@@ -3,13 +3,33 @@ import {
   NDivider,
   NGradientText,
   NImage,
+  NPopselect,
+  NProgress,
   NResult,
+  NSwitch,
+  NTag,
+  NText,
+  NTimeline,
+  NTimelineItem,
   create,
 } from 'naive-ui'
 import type { UserModule } from '~/types'
 
 const naive = create({
-  components: [NAvatar, NImage, NResult, NGradientText, NDivider],
+  components: [
+    NAvatar,
+    NImage,
+    NResult,
+    NPopselect,
+    NProgress,
+    NGradientText,
+    NDivider,
+    NSwitch,
+    NText,
+    NTag,
+    NTimeline,
+    NTimelineItem,
+  ],
 })
 
 export const install: UserModule = ({ use }) => {
